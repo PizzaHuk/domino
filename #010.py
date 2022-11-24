6c7aafe33347df6118bc8a1232b2bf89b53b90ad
PizzaHuk <as0202jk1234@gmail.com>
2022-11-25 00:06:46 +0900

    #000~#010

---
+++ #010.py
@@ -0,0 +1,12 @@
+#001~010
+
+print("Hello World") #001
+print("Mary's cosmetics") #002
+print('신씨가 소리질렀다. "도둑이야".') #003
+print("C:\Windows") #004
+print("안녕하세요.\n만나서\t\t반갑습니다.") #005 /n = 줄바꿈 /t = 탭
+print("오늘은","일요일") #006 오늘은 일요일
+print("naver","kakao","sk","samsung", sep=";") #007
+print("naver","kakao","sk","samsung", sep="/") #008
+print("first", end="");print("second")
+print(5/3) #010
